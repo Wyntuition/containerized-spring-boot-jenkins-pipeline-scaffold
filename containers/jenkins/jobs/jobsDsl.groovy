47e88e9566c68d220890dd319859c3778e9ed987
Wyn Van Devanter <wyn.vandevanter@excella.com>
2018-01-08 14:28:18 -0500

    prevent mb pipeline from building new branches as found

--- containers/jenkins/jobs/jobsDsl.groovy
+++ containers/jenkins/jobs/jobsDsl.groovy
@@ -1,8 +1,17 @@
-multibranchPipelineJob('vis-case-processing-etl-nonprod') {
+multibranchPipelineJob('vis-case-processing-etl-nonprod-test') {
     branchSources {
         git {
-            remote('git@git.uscis.dhs.gov:Wyntuition/containerized-spring-boot-jenkins-pipeline-scaffold')
-            excludes('production-*')
+            remote('git@git.uscis.dhs.gov:USCIS/vis-case-processing-etl')
+            excludes('master production-*')
+        }
+    }
+    configure { project ->
+        project / 'sources' / 'data' / 'jenkins.branch.BranchSource'/ strategy(class: 'jenkins.branch.DefaultBranchPropertyStrategy') {
+            properties(class: 'java.util.Arrays$ArrayList') {
+                a(class: 'jenkins.branch.BranchProperty-array'){
+                    'jenkins.branch.NoTriggerBranchProperty'()
+                }
+            }
         }
     }
     triggers {
